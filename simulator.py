52631fd5c8b2452d8c6cece94dce3624ded3116e
krmnino <kurt.manrique.n@gmail.com>
2020-04-25 02:33:42 -0400

    refactored command_line function, difference() needs implementation

--- simulator.py
+++ simulator.py
@@ -41,6 +41,10 @@ def logistic_fn(population):
         day_counter += 1
     return (days, logistic)
 
+def difference(parsed_data, day1, day2):
+    for col in parsed_data:
+        print(col)
+
 def projection(next_days, days_passed, cases, growth_factor):
     total_cases = cases[len(cases)-1]
     counter = 0
@@ -80,104 +84,26 @@ def print_data(days, dates, cases, growth_factor):
 
 def command_line():
     parsed_data = 0
+    instructions = ['load', 'show', 'delete', ]
     while(True):
-        input_cmd = input(">> ")
+        input_cmd = input('>> ')
         parsed_input = input_cmd.split()
-        if(parsed_input[0] == "exit"):
-            print("Exiting...")
+        if(parsed_input[0] == 'exit'):
+            print('Exiting...')
             break
         if(parsed_input[0] == 'help'):
             print('usage manual:')
             print('load [FILE]                                      load data set in memory')
             print('show                                             displays loaded data set')
+            print('show_all [next_days] [avg_previous_days]         displays loaded data set and projection')
             print('delete                                           erase data set loaded in memory')
+            print('diff                                             shows difference of values between 2 days')
             print('projection [next_days] [avg_previous_days]       show projection for the next x days using avg growth factor from y previous days')
             print('plot_cases                                       display cases graph')
             print('plot_cases_log                                   display cases in a logarithmic graph')
             print('plot_growth                                      display growth rate graph')
             print('plot_growth [from_day][to_day]                   display growth rate graph from a range of days')
             print('clear                                            clears the console')
-            continue
-
-        if(parsed_input[0] == "load" and len(parsed_input) != 2):
-            print("usage: load [FILE PATH]")
-            continue
-        elif(parsed_input[0] == "load" and len(parsed_input) == 2):
-            try:
-                input_data = open(str(parsed_input[1]))
-                parsed_data = parse_file(input_data)
-                input_data.close()
-                print("Loaded data from", parsed_input[1])
-            except:
-                print(parsed_input[1], "is not accesible")
-            continue
-                
-        if(parsed_input[0] == "show" and parsed_data == 0):
-            print("data has not been loaded into memory")
-            continue
-        elif(parsed_input[0] == "show" and parsed_data != 0):
-            print_data(parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3])
-            continue
-
-        if(parsed_input[0] == "show_all" and parsed_data == 0):
-            print("data has not been loaded into memory")
-            continue
-        elif(parsed_input[0] == "show_all" and len(parsed_input) != 3):
-            print("usage: show_all [next_days] [avg_previous_days]")
-            continue
-        elif(parsed_input[0] == "show_all" and parsed_data != 0):
-            print_data(parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3])
-            projection(int(parsed_input[1]), int(parsed_input[2]), parsed_data[2], parsed_data[3])
-            continue
-        
-        if(parsed_input[0] == "delete"):
-            parsed_data = 0
-            continue
-
-        if(parsed_input[0] == "projection" and parsed_data == 0):
-            print("data has not been loaded into memory")
-            continue
-        elif(parsed_input[0] == "projection" and len(parsed_input) != 3):
-            print("usage: projection [next_days] [avg_previous_days]")
-            continue
-        elif(parsed_input[0] == "projection" and len(parsed_input) == 3 and parsed_data != 0):
-            projection(int(parsed_input[1]), int(parsed_input[2]), parsed_data[2], parsed_data[3])
-            continue
-
-        if(parsed_input[0] == "plot_cases" and parsed_data == 0):
-            print("data has not been loaded into memory")
-            continue
-        elif(parsed_input[0] == "plot_cases" and parsed_data != 0):
-            plot_graph(parsed_data[0], parsed_data[2], 'b', "Days", "Cases")
-            continue
-
-        if(parsed_input[0] == "plot_cases_log" and parsed_data == 0):
-            print("data has not been loaded into memory")
-            continue
-        elif(parsed_input[0] == "plot_cases_log" and parsed_data != 0):
-            plot_graph_log(parsed_data[0], parsed_data[2], 'b', "Days", "Cases")
-            continue
-
-        if(parsed_input[0] == "plot_growth" and parsed_data == 0):
-            print("data has not been loaded into memory")
-            continue
-        elif(parsed_input[0] == "plot_growth" and parsed_data != 0 and len(parsed_input) == 1):
-            plot_graph(parsed_data[0], parsed_data[3], 'k', "Days", "Growth Ratio (%)")  
-            continue
-        elif(parsed_input[0] == "plot_growth" and len(parsed_input) != 3):
-            print(len(parsed_input))
-            print("usage: plot_growth")
-            print("       plot_growth [from day] [to_day]")
-            continue
-        elif(parsed_input[0] == "plot_growth" and int(parsed_input[2]) > parsed_data[0][len(parsed_data[0]) - 1]):
-            print("range of days is invalid, starting day must be less than ending day")
-            continue
-        elif(parsed_input[0] == "plot_growth" and int(parsed_input[1]) > int(parsed_input[2])):
-            print("range of days is invalid, starting day must be less than ending day")
-            continue
-        elif(parsed_input[0] == "plot_growth" and parsed_data != 0):
-            plot_graph(parsed_data[0][int(parsed_input[1]):int(parsed_input[2]) + 1], \
-                parsed_data[3][int(parsed_input[1]):int(parsed_input[2]) + 1], 'k', "Days", "Growth Ratio (%)")  
             continue
 
         if(parsed_input[0] == "clear"):
@@ -188,8 +114,87 @@ def command_line():
                 os.system("clear")
                 continue
             continue
-        else:
-            print('Invalid input. For instructions type "help".')
+
+        if(parsed_input[0] == 'load'):
+            if(len(parsed_input) != 2):
+                print("usage: load [FILE PATH]")
+                continue
+            else:
+                try:
+                    input_data = open(str(parsed_input[1]))
+                    parsed_data = parse_file(input_data)
+                    input_data.close()
+                    print("Loaded data from", parsed_input[1])
+                except:
+                    print(parsed_input[1], "is not accesible")
+                continue
+
+        if(parsed_input[0] == 'delete'):
+            parsed_data = 0
+            continue
+
+        if(parsed_data == 0):
+            print('data has not been loaded into memory')
+            continue
+
+        if(parsed_input[0] == 'show'):
+            print_data(parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3])
+            continue
+
+        if(parsed_input[0] == 'show_all'):
+            if(len(parsed_input) != 3):
+                print("usage: show_all [next_days] [avg_previous_days]")
+                continue
+            else:
+                print_data(parsed_data[0], parsed_data[1], parsed_data[2], parsed_data[3])
+                projection(int(parsed_input[1]), int(parsed_input[2]), parsed_data[2], parsed_data[3])
+                continue
+
+        if(parsed_input[0] == 'diff'):
+            if(len(parsed_input) != 3):
+                print("usage: diff [from_day] [to_day]")
+                continue
+            else:
+                difference(parsed_data, int(parsed_input[1]), int(parsed_input[2]))
+                continue
+
+        if(parsed_input[0] == 'projection'):
+            if(len(parsed_input) != 3):
+                print("usage: projection [next_days] [avg_previous_days]")
+                continue
+            else:
+                projection(int(parsed_input[1]), int(parsed_input[2]), parsed_data[2], parsed_data[3])
+                continue
+
+        if(parsed_input[0] == 'plot_cases'):
+            plot_graph(parsed_data[0], parsed_data[2], 'b', "Days", "Cases")
+            continue
+
+        if(parsed_input[0] == 'plot_cases_log'):
+            plot_graph_log(parsed_data[0], parsed_data[2], 'b', "Days", "Cases")
+            continue
+
+        if(parsed_input[0] == 'plot_growth'):
+            if(len(parsed_input) == 1):
+                plot_graph(parsed_data[0], parsed_data[3], 'k', "Days", "Growth Ratio (%)")  
+                continue
+            elif(len(parsed_input) != 3):
+                print(len(parsed_input))
+                print("usage: plot_growth")
+                print("       plot_growth [from day] [to_day]")
+                continue
+            elif(parsed_input[0] == "plot_growth" and int(parsed_input[2]) > parsed_data[0][len(parsed_data[0]) - 1]):
+                print("range of days is invalid, starting day must be less than ending day")
+                continue
+            elif(parsed_input[0] == "plot_growth" and int(parsed_input[1]) > int(parsed_input[2])):
+                print("range of days is invalid, starting day must be less than ending day")
+                continue
+            else:
+                plot_graph(parsed_data[0][int(parsed_input[1]):int(parsed_input[2]) + 1], \
+                    parsed_data[3][int(parsed_input[1]):int(parsed_input[2]) + 1], 'k', "Days", "Growth Ratio (%)")  
+                continue
+        print('Invalid input. For instructions type "help".')
+        
     
 #################################################################################
 
