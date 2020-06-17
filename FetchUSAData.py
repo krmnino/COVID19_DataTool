a9451b0ddd16d5b7a0a84ba4e8f868415557f4d3
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-06-17 02:23:41 -0400

    Stopped using OWID database, instead use covidtracking for USA data only

---
+++ FetchUSAData.py
@@ -0,0 +1,36 @@
+import os
+import requests
+import time
+from datetime import date
+from DataPoint import DataPoint
+
+
+def fetch_data_usa():
+    src_url = 'https://covidtracking.com/api/v1/us/daily.csv'
+    try:
+        req_data = requests.get(src_url).text
+    except:
+        print('Could not rearch USA data source.')
+    req_data = requests.get(src_url).text
+    out = open('fetch/raw_USA.csv', 'w')
+    out.write(req_data)
+    out.close()
+
+def diff_raw_USA_data():
+    try:
+        open('fetch/raw_USA.csv')
+    except:
+        print('Could not access USA raw data file.')
+    else:
+        country_file = open('data/USA_data.csv')
+        raw_data_file = open('fetch/raw_USA.csv', 'r')
+        last_line = ''
+        for line in country_file:
+            last_line = line
+        last_date = last_line[:last_line.find(',')]
+        new_data = []
+        for i, line in enumerate(raw_data_file):
+            if(i == 0):
+                continue
+            print(line.split(',')[0])
+        return new_data
