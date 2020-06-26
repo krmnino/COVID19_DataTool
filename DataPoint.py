ccec07ae867af03dd7b5963535bd2946a9aaa0ba
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-06-26 03:04:33 -0400

    updated DataPoint structure to allow recovered and hospitalized data

--- DataPoint.py
+++ DataPoint.py
@@ -3,22 +3,28 @@ class DataPoint:
     cases = 0
     deaths = 0
     tests = 0
+    recovered = 0
+    hospitalized_= 0
 
-    def __init__(self, date_, cases_, deaths_, tests_):
+    def __init__(self, date_, cases_, deaths_, tests_, recovered_, hospitalized_):
         self.date = date_
         self.cases = cases_
         self.deaths = deaths_
         self.tests = tests_
+        self.recovered = recovered_
+        self.hospitalized = hospitalized_
     
     def get_data(self):
-        return [self.date, self.cases, self.deaths, self.tests]
+        return [self.date, self.cases, self.deaths, self.tests, self.recovered, self.hospitalized]
 
     def show(self):
         #print('%12s'%(str(self.iso_code)), '%12s'%(str(self.date)), '%12s'%(str(self.cases), '%12s'%(str(self.deaths), '%12s'%(str(self.tests)))))
         print('%15s'%(str(self.date)), end = '')
         print('%12s'%(str(self.cases)), end = '')
         print('%12s'%(str(self.deaths)), end = '')
-        print('%12s'%(str(self.tests)))
+        print('%12s'%(str(self.tests)),  end = '')
+        print('%12s'%(str(self.recovered)),  end = '')
+        print('%12s'%(str(self.hospitalized)))
 
 
 
